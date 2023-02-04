c381e88e6c04b918273bc35f8a53d0d65f3972ef
colton <colton.o@hotmail.com>
2023-02-04 10:30:01 +0800

    omg we got a root

--- Assets/Scripts/root_handler.cs
+++ Assets/Scripts/root_handler.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 public class root_handler : MonoBehaviour
 {
-   void Start()
+    Texture2D texture;
+    Sprite sprite;
+    void Start()
     {
-        Texture2D texture = new Texture2D(128, 128);
-        Sprite sprite = Sprite.Create(texture,new Rect(0,0,128,128), Vector2.zero);
+        texture = new Texture2D(Screen.width, Screen.height);
+        sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
         GetComponent<SpriteRenderer>().sprite = sprite;
         for (int y = 0; y < texture.height; y++)
         {
             for (int x = 0; x < texture.width; x++)
             {
-                Color color = ((x & y) != 0 ? Color.magenta : Color.black);
-                texture.SetPixel(x, y, color);
+                texture.SetPixel(x, y, Color.black);
             }
         }
         texture.Apply();
     }
-    
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            draw_pix(500, 800, 500, 1000, Color.cyan);
+        }
+    }
+    void draw_pix(int x_start, int x_end, int y_start, int y_end, Color col)
+    {
+
+        int x = x_start;
+        int y = y_start;
+
+        while (x != x_end && y != y_end)
+        {
+            texture.SetPixel(x, y, col);
+            if (x_end > x)
+            {
+                x += 1;
+            }
+            else if (x_end < x)
+            {
+                x += -1;
+            }
+
+
+            if (y_end > y)
+            {
+                y += 1;
+            }
+            else if (y_end < y)
+            {
+                y += -1;
+            }
+
+        }
+        texture.Apply();
+    }
 }
 
 
-     
