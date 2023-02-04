a9b830e926e05d697f556a9c1492b5b8372afb42
colton <colton.o@hotmail.com>
2023-02-04 17:46:34 +0800

    added root bool

--- Assets/Scripts/root_handler.cs
+++ Assets/Scripts/root_handler.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class root_handler : MonoBehaviour
 {
-    const int PIXEL_THRESHOLD = 5;
-    const int Dir_distance = 10;
-    const int grad_min = 1;
-    const int grad_max = 25;
+    const int PIXEL_THRESHOLD = 25;
+    const int Dir_distance = 25;
+    const int grad_min = 0;
+    const int grad_max = 5;
+    const int min_length = 3;
+    const int max_length = 7;
     Texture2D texture;
     Sprite sprite;
     Vector2 root_start, root_end;
     LineRenderer drag;
+
+    bool can_root;
+
 
     enum Type
     {
@@ -43,6 +48,17 @@ public class root_handler : MonoBehaviour
 
     void Update()
     {
+        if (can_root)
+        {
+            drag.startColor = Color.green;
+            drag.endColor = Color.blue;
+        }
+        else
+        {
+            drag.startColor = Color.red;
+            drag.endColor = Color.yellow;
+        }
+
         Vector3 mouse_world = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetButtonDown("Fire1"))
         {
@@ -54,15 +70,23 @@ public class root_handler : MonoBehaviour
             drag.SetPosition(1, new Vector3(mouse_world.x, mouse_world.y, -2));
             root_end = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
+            if (root_end.y >= root_start.y)
+                can_root = false;
+            else
+                can_root = true;
         }
         if (Input.GetButtonUp("Fire1"))
         {
+            if (can_root)
+            {
+                StartCoroutine(draw_root(Mathf.RoundToInt(root_start.x),
+                                Mathf.RoundToInt(root_end.x),
+                                Mathf.RoundToInt(root_start.y),
+                                Mathf.RoundToInt(root_end.y),
+                                Color.cyan));
+            }
 
-            StartCoroutine(draw_root(Mathf.RoundToInt(root_start.x),
-                Mathf.RoundToInt(root_end.x),
-                Mathf.RoundToInt(root_start.y),
-                Mathf.RoundToInt(root_end.y),
-                Color.cyan));
+
 
             drag.SetPosition(0, Vector3.zero);
             drag.SetPosition(1, Vector3.zero);
@@ -70,7 +94,6 @@ public class root_handler : MonoBehaviour
     }
     IEnumerator draw_root(int x_start, int x_end, int y_start, int y_end, Color col)
     {
-        int loopnum = 0;
 
         Vector2 dir = new Vector2(0, 0);
         Vector2 gradient = new Vector2(0, 0);
@@ -82,74 +105,100 @@ public class root_handler : MonoBehaviour
         //main draw loop
         while (Mathf.Abs(x - x_end) > PIXEL_THRESHOLD || Mathf.Abs(y - y_end) > PIXEL_THRESHOLD)
         {
+            int loop_save = 99999999;
 
             //pick length
-            int length = Random.Range(5, 10);
+            int length = Random.Range(min_length, max_length);
 
             //set direction and gradient
             if (y_end > y)
             {
-                dir.y = 1;
+                dir.y = Random.Range(0, 2);
                 gradient.y = Random.Range(grad_min, grad_max);
             }
             else if (y_end < y)
             {
-                dir.y = -1;
+                dir.y = Random.Range(-1, 1);
+                gradient.y = Random.Range(grad_min, grad_max);
+            }
+            else
+            {
+                dir.y = Random.Range(-1, 2);
                 gradient.y = Random.Range(grad_min, grad_max);
             }
 
 
             if (x_end > x)
             {
-                dir.x = 1;
+                dir.x = Random.Range(0, 2);
                 gradient.x = Random.Range(grad_min, grad_max);
             }
             else if (x_end < x)
             {
-                dir.x = -1;
+                dir.x = Random.Range(-1, 1);
+                gradient.x = Random.Range(grad_min, grad_max);
+            }
+            else
+            {
+                dir.x = Random.Range(-1, 2);
                 gradient.x = Random.Range(grad_min, grad_max);
             }
 
 
-            int grad_dir = Random.Range(0, 1);
-            if (grad_dir == 1)
+            int grad_dir = Random.Range(0, 2);
+            Debug.Log("GRAD =" + grad_dir);
+
+            if (grad_dir == 1 || Mathf.Abs(y - y_end) < Dir_distance)
                 gradient.y = 1;
-            else
+            if (grad_dir == 0 || Mathf.Abs(x - x_end) < Dir_distance)
                 gradient.x = 1;
 
 
             //run through length
             while (length != 0)
             {
-                //if (Mathf.Abs(x - x_end) < Dir_distance || Mathf.Abs(y - y_end) < Dir_distance)
-                // break;
+                if (Mathf.Abs(x - x_end) < Dir_distance && Mathf.Abs(y - y_end) < Dir_distance)
+                    break;
                 //per gradient render
                 for (int gx = 0; gx <= gradient.x; gx++)
                 {
                     x += (int)dir.x;
                     Debug.Log(dir);
-                    texture.SetPixel(x, y, col);
+                    pixel_set(x, y, col);
                     texture.Apply();
-                    yield return new WaitForEndOfFrame();
+                    yield return new WaitForSeconds(0.0000001f);
                 }
                 for (int gy = 0; gy <= gradient.y; gy++)
                 {
 
                     y += (int)dir.y;
-                    texture.SetPixel(x, y, col);
+                    pixel_set(x, y, col);
                     texture.Apply();
-                    yield return new WaitForEndOfFrame();
+                    yield return new WaitForSeconds(0.0000001f);
                 }
                 length--;
             }
-            loopnum += 1;
-
-            if (loopnum > 10000)
-                break;
+            loop_save--;
+            if (loop_save <= 0)
+            {
+                Debug.Log("omg you have infinate loop");
+            }
         }
 
-        Debug.Log("BREAK");
+        Debug.Log("BREAK \n" + "x = " + x + "x end =" + x_end + "y = " + y + "y end = " + y_end);
     }
+
+    void pixel_set(int x, int y, Color col)
+    {
+        texture.SetPixel(x, y, col);
+        texture.SetPixel(x + 1, y, col);
+        texture.SetPixel(x - 1, y, col);
+        texture.SetPixel(x, y + 1, col);
+        texture.SetPixel(x, y - 1, col);
+    }
+
 }
+
+
 
 
